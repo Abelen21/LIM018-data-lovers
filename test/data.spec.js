b3d5e3cf691c49706e3e2b84cab88a9bd6990080
belen <anabelenarroyo21@gmail.com>
2022-07-11 19:12:03 -0500

    test ordenar

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,6 @@
 // import { filtrarPersonajes } from '../src/data.js';
 
-import { obtenerPersonajes } from '../src/data.js';
+import { obtenerPersonajes, ordenarPersonajes } from '../src/data.js';
 
 // const arreglo = [{specie: 'Cat'},{specie: 'pizza'}]
 
@@ -15,24 +15,32 @@ import { obtenerPersonajes } from '../src/data.js';
 // });
 
 
-describe('obtenerPersonajes', () => {
+describe('probando obtenerPersonajes', () => {
   it('is a function', () => {
     expect(typeof obtenerPersonajes).toBe('function');
   });
-
   it('returns characters', () => {
     const films = [{'people': [{'name': 'B'}]},{'people': [{'name': 'A'}]}]
     expect(obtenerPersonajes(films)).toEqual([{'name': 'B'},{'name': 'A'}]);
   });
+
 })
 
 
-///para la funcion ordenar hay que llamara la funcion xq no retorna algo
-// it('returns characters', () => {
-//   const films = [{'people': [{'name': 'B'}]},{'people': [{'name': 'A'}]}]
-//   ordenarPersonajes(personajes)
-//   expect(personajes).toEqual
-// expect(obtenerPersonajes(films)).toEqual([{'name': 'B'},{'name': 'A'}]);
+describe('probando ordenarPersonajes', () =>{
+  it('is a function', () => {
+    expect(typeof ordenarPersonajes).toBe('function');
+  });
+  it('ordena personajes',()=>{
+    // Dado
+    const personajes = [{'name': 'B'},{'name': 'A'}]
+    // Cuando
+    ordenarPersonajes(personajes)
+    // Entonces
+    expect(personajes).toEqual([{'name': 'A'},{'name': 'B'}])
+  })
+})
+
 
 
 
