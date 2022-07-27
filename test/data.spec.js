9275315d148cb03f7fd82ff1215f2b5d32df5016
belen <anabelenarroyo21@gmail.com>
2022-07-26 21:30:25 -0500

    revisado test ordenar

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,75 +1,55 @@
-// import { filtrarPersonajes } from '../src/data.js';
 
-import { ordenarAZ, obtenerPersonajes, ordenarPersonajes } from '../src/data.js';
-
-// const arreglo = [{specie: 'Cat'},{specie: 'pizza'}]
-
-// describe('filtrarPersonajes', () => {
-//   it('is a function', () => {
-//     expect(typeof filtrarPersonajes).toBe('function');
-//   });
-
-//   it('should filter by cat', () => {
-//     expect(filtrarPersonajes(arreglo)).toEqual([{specie:'Cat'}]);
-//   });
-// });
-
+import { obtenerDataTipo, ordenarPersonajes } from '../src/data.js';
 
 ///  Tests funciones animaciones ///////
 
-describe('probando ordenarAZ', () => {
-  it('is a function', () => {
-    expect(typeof ordenarAZ).toBe('function');
-  });
-  it('retorna la data ordenada', () => {
-    // dado de la A la Z
-    const mockOrder= "A-Z"
-    // cuando
-    const sort =ordenarAZ(mockData,mockOrder)
-    // entonces;
-    expect(sort.films[3].title).toBe('Zastle in the Sky')
-  });
-  
-});
+// describe('probando ordenarAZ', () => {
+//   it('is a function', () => {
+//     expect(typeof ordenarAZ).toBe('function');
+//   });
+//   it('retorna la data ordenada', () => {
+//     // dado de la A la Z
+//     const mockOrder= "A-Z"
+//     // cuando
+//     const sort =ordenarAZ(mockData,mockOrder)
+//     // entonces;
+//     expect(sort.films[3].title).toBe('Zastle in the Sky')
+//   });
+// });
 
-describe('probando ordenarZA', () => {
-  it('is a function', () => {
-    expect(typeof ordenarAZ).toBe('function');
-  });
-  it('retorna la data ordenada', () => {
-    // dado de la A la Z
-    const mockOrder= "Z-A"
-    // cuando
-    const sort =ordenarAZ(mockData,mockOrder)
-    // const expectedArray = "films": [{}]
-    // entonces;
-    expect(sort[0].title).toBe('My Neighbor Totoro')
-    expect(sort[1].title).toBe("Kiki's Delivery Service")
-    expect(sort[2].title).toBe('Grave of the Fireflies')
-    expect(sort[3].title).toBe('Castle in the Sky')
-
-
-  });
-  
-});
-
-
+// describe('probando ordenarZA', () => {
+//   it('is a function', () => {
+//     expect(typeof ordenarAZ).toBe('function');
+//   });
+//   it('retorna la data ordenada', () => {
+//     const mockOrder= "Z-A"
+    
+//     const sort =ordenarAZ(mockData,mockOrder)
+//     // entonces;
+//     expect(sort[0].title).toBe('My Neighbor Totoro')
+//     expect(sort[1].title).toBe("Kiki's Delivery Service")
+//     expect(sort[2].title).toBe('Grave of the Fireflies')
+//     expect(sort[3].title).toBe('Castle in the Sky')
+//   });
+// });
 
 
 
 /// Tests funciones personajes //////
 
-describe('probando obtenerPersonajes', () => {
+describe('probando obtenerDataTipo', () => {
   it('is a function', () => {
-    expect(typeof obtenerPersonajes).toBe('function');
+    expect(typeof obtenerDataTipo).toBe('function');
   });
   it('returns characters', () => {
     const films = [{'people': [{'name': 'B'}]},{'people': [{'name': 'A'}]}]
-    expect(obtenerPersonajes(films)).toEqual([{'name': 'B'},{'name': 'A'}]);
+    expect(obtenerDataTipo(films,'personajes')).toEqual([{'name': 'B'},{'name': 'A'}]);
   });
-
+  it('returns locations', () => {
+    const films = [{'locations': [{'name': 'B'}]},{'locations': [{'name': 'A'}]}]
+    expect(obtenerDataTipo(films,'locaciones')).toEqual([{'name': 'B'},{'name': 'A'}]);
+  });
 })
-
 
 describe('probando ordenarPersonajes', () =>{
   it('is a function', () => {
